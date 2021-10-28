2bb4f5e0f0a0ed8d539e12700dd6f341edc67156
magnuswenzerSMHI <magnus.wenzer@smhi.se>
2021-10-28 16:50:19 +0200

    saves and defaults

--- saves.py
+++ saves.py
@@ -5,10 +5,38 @@ import pathlib
 import json
 
 
+def get_default_users():
+    users = []
+    for path in pathlib.Path(pathlib.Path(__file__).parent, 'defaults').iterdir():
+        if path.suffix != '.yaml':
+            continue
+        users.append(path.stem)
+    return sorted(users)
+
+
+def get_default_user_file_path(user):
+    users = get_default_users()
+    if user not in users:
+        return False
+    return pathlib.Path(pathlib.Path(__file__).parent, 'defaults', f'{user}.yaml')
+
+
 class Defaults:
 
-    def __init__(self):
-        self.file_path = pathlib.Path(pathlib.Path(__file__).parent, 'defaults.yaml')
+    def __init__(self, user=None):
+        self._this_directory = pathlib.Path(__file__).parent
+        self._default_user_path = pathlib.Path(self._this_directory, 'default.user')
+
+        self.file_path = None
+        if not user:
+            user = self._load_default_user()
+        if not user:
+            user = 'default'
+
+        self.file_path = get_default_user_file_path(user)
+        if not self.file_path:
+            raise Exception('Invalid user default path')
+        self._save_default_user(user)
 
         self.data = {}
 
@@ -19,12 +47,24 @@ class Defaults:
         Loads dict from json
         :return:
         """
-        if self.file_path.exists():
+        if self.file_path and self.file_path.exists():
             with open(self.file_path) as fid:
                 self.data = yaml.load(fid, Loader=SafeLoader)
 
     def get(self, key, default=None):
         return self.data.get(key, default)
+
+    def _save_default_user(self, user):
+        if not user:
+            return
+        with open(self._default_user_path, 'w') as fid:
+            fid.write(user)
+
+    def _load_default_user(self):
+        if not self._default_user_path.exists():
+            return
+        with open(self._default_user_path) as fid:
+            return fid.read().strip()
 
 
 class Saves:
@@ -83,8 +123,10 @@ class SaveSelection:
                     pass
         self._saves.set(self._saves_id_key, data)
 
-    def load_selection(self):
+    def load_selection(self, default_user=None, **kwargs):
         data = self._saves.get(self._saves_id_key)
+        if default_user:
+            self._defaults = Defaults(user=default_user)
         if type(self._selections_to_store) == dict:
             for name, comp in self._selections_to_store.items():
                 try:
